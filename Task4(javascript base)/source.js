0ebc9476055a3e00c6dbde1f163225dcc66e3391
Sliunkova Valeriya <valerijaslu@gmail.com>
2018-03-06 14:00:41 +0300

    all comments have been corrected

--- Task4(javascript base)/source.js
+++ Task4(javascript base)/source.js
@@ -1,4 +1,4 @@
-﻿(function () {
+﻿var module = (function () {
     var photoPosts = [
         {
             id:'1',
@@ -6,6 +6,7 @@
             createdAt: new Date('2019-03-01T17:30:00'),
             author: 'Alex',
             photoLink: 'image/photo1.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'book'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -16,6 +17,7 @@
             createdAt: new Date('2018-03-01T17:31:00'),
             author: 'Julia',
             photoLink: 'image/photo2.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'book'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -26,6 +28,7 @@
             createdAt: new Date('2018-03-01T17:32:00'),
             author: 'Jess',
             photoLink: 'image/photo3.jpeg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -36,6 +39,7 @@
             createdAt: new Date('2018-03-01T17:33:00'),
             author: 'Marina',
             photoLink: 'image/photo4.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -46,6 +50,7 @@
             createdAt: new Date('2018-03-01T17:34:00'),
             author: 'Regina',
             photoLink: 'image/photo5.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -56,6 +61,7 @@
             createdAt: new Date('2018-03-01T17:35:00'),
             author: 'Cece',
             photoLink: 'image/photo6.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -66,6 +72,7 @@
             createdAt: new Date('2018-03-01T17:36:00'),
             author: 'Alex',
             photoLink: 'image/photo7.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -76,6 +83,7 @@
             createdAt: new Date('2018-03-01T17:37:00'),
             author: 'Lexie',
             photoLink: 'image/photo8.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -86,6 +94,7 @@
             createdAt: new Date('2018-03-01T17:38:00'),
             author: 'Nick',
             photoLink: 'image/photo9.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -96,6 +105,7 @@
             createdAt: new Date('2018-03-01T17:39:00'),
             author: 'Nick',
             photoLink: 'image/photo10.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -106,6 +116,7 @@
             createdAt: new Date('2018-04-01T17:30:00'),
             author: 'Joe',
             photoLink: 'image/photo11.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -116,6 +127,7 @@
             createdAt: new Date('2018-05-01T17:30:00'),
             author: 'Sam',
             photoLink: 'image/photo12.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -126,6 +138,7 @@
             createdAt: new Date('2018-06-01T17:30:00'),
             author: 'Clover',
             photoLink: 'image/photo13.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -136,6 +149,7 @@
             createdAt: new Date('2018-07-01T17:30:00'),
             author: 'Garry',
             photoLink: 'image/photo14.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -146,6 +160,7 @@
             createdAt: new Date('2018-08-01T17:30:00'),
             author: 'Henry',
             photoLink: 'image/photo15.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -156,6 +171,7 @@
             createdAt: new Date('2018-09-01T17:30:00'),
             author: 'Ivan',
             photoLink: 'image/photo16.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -166,6 +182,7 @@
             createdAt: new Date('2018-10-01T17:30:00'),
             author: 'Barney',
             photoLink: 'image/photo17.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -176,6 +193,7 @@
             createdAt: new Date('2018-11-01T17:30:00'),
             author: 'Robin',
             photoLink: 'image/photo18.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -186,6 +204,7 @@
             createdAt: new Date('2018-11-01T17:30:00'),
             author: 'Ted',
             photoLink: 'image/photo19.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         },
@@ -196,10 +215,20 @@
             createdAt: new Date('2018-12-01T17:30:00'),
             author: 'Taylor',
             photoLink: 'image/photo20.jpg',
+            state: 'active',
             hashTags: ['summer', 'love', 'style'],
             likes: ['Marina', 'Regina', 'Cece', 'Nick']
         }
     ];
+
+    var schema = {
+        id: 'String',
+        description: 'String',
+        createdAt: 'Date',
+        author: 'String',
+        photoLink: 'String',
+        state: 'String'
+    };
 
     //comparator
     function compareTo(a, b){
@@ -215,21 +244,15 @@
     }
 
     //get post from array
-    function getPhotoPost(str){
-        for (var i = 0; i < photoPosts.length; i++) {
-            if(photoPosts[i].id === str){
-                return photoPosts[i];
-            }
-        }
-        return null;
+    function getPhotoPost(postID){
+        return photoPosts.find(function (x) { return x.id === postID; });
     }
 
-
     //remove post from array
-    function removePhotoPost(str) {
+    function removePhotoPost(postID) {
         for (var i = 0; i < photoPosts.length; i++) {
-            if(photoPosts[i].id === str){
-                photoPosts.splice(i, 1);
+            if(photoPosts[i].id === postID){
+                photoPosts[i].state = 'deleted';
                 return true;
             }
         }
@@ -237,19 +260,22 @@
     }
 
     //validate
-    function validatePhotoPost(Object){
-        if(Object.id == null || Object.description == null || Object.createdAt == null || Object.author == null || Object.photoLink == null){
-            return false;
+    function validatePhotoPost(object){
+        for(var item in schema){
+            if(!object.hasOwnProperty(item)){
+                return false;
+            }
+            if(object[item].constructor.name !== schema[item]){
+                return false;
+            }
         }
-        else{
-            return true;
-        }
+        return true;
     }
     
     //add post to array
-    function addPhotoPost(Object) {
-        if(validatePhotoPost(Object)){
-            photoPosts.push(Object);
+    function addPhotoPost(object) {
+        if(validatePhotoPost(object)){
+            photoPosts.push(object);
             return true;
         }
         else {
@@ -258,26 +284,13 @@
     }
 
     //edit post
-    function editPhotoPost(str, Object) {
-        var post = getPhotoPost(str);
+    function editPhotoPost(postID, object) {
+        var post = getPhotoPost(postID);
         if(post != null){
-            if(Object.description != null){
-                post.description = Object.description;
-            }
-            if(Object.photoLink != null){
-                post.photoLink = Object.photoLink;
-            }
-            if(Object.hashTags != null){
-                post.hashTags.length = 0;
-                for(var i = 0; i < Object.hashTags.length; i++){
-                    post.hashTags[i] = Object.hashTags[i];
-                }
-            }
-            if(post.description != null && post.photoLink != null){
-                removePhotoPost(str);
-                addPhotoPost(post);
-                return true;
-            }
+            Object.assign(post, object);
+            removePhotoPost(postID);
+            addPhotoPost(post);
+            return true;
         }
         return false;
     }
@@ -294,111 +307,99 @@
 
     //get posts
     function getPhotoPosts(skip, top, filterConfig) {
-        var toShow = [];
+        var toShow = photoPosts.filter(function (x) { return x.state !== 'deleted'; });
 
         if(arguments.length === 2){
-            photoPosts.sort(compareTo);
-            return photoPosts.slice(skip, skip + top);
+            toShow.sort(compareTo);
+            return toShow.slice(skip, skip + top);
         }
 
         if(arguments.length === 3){
-            for(var i = 0; i < photoPosts.length; i++){
-                if(filterConfig.author != null){
-                    if(filterConfig.createdAt != null){
-                        if(filterConfig.hashTags != null){
-                            if(isCommonHashTags(photoPosts[i], filterConfig.hashTags) && photoPosts[i].author === filterConfig.author && photoPosts[i].createdAt.getDate() === filterConfig.createdAt.getDate() && photoPosts[i].createdAt.getTime() === filterConfig.createdAt.getTime()){
-                                toShow.push(photoPosts[i]);
-                            }
-                        }
-                        else {
-                            if(photoPosts[i].author === filterConfig.author && photoPosts[i].createdAt.getDate() === filterConfig.createdAt.getDate() && photoPosts[i].createdAt.getTime() === filterConfig.createdAt.getTime()){
-                                toShow.push(photoPosts[i]);
-                            }
-                        }
-                    }
-                    else if(filterConfig.hashTags != null){
-                        if(isCommonHashTags(photoPosts[i], filterConfig.hashTags) && photoPosts[i].author === filterConfig.author){
-                            toShow.push(photoPosts[i]);
+            if(filterConfig.author){
+                toShow = toShow.filter(function (x) { return x.author === filterConfig.author; });
+            }
+            if(filterConfig.createdAt){
+                toShow = toShow.filter(function (x) { return (x.createdAt.getTime() === filterConfig.createdAt.getTime() && x.createdAt.getDate() === filterConfig.createdAt.getDate()); });
+            }
+            if(filterConfig.hashTags){
+                toShow = toShow.filter(function (x) {
+                    for(var i = 0 ; i < filterConfig.hashTags.length; i++){
+                        if(x.hashTags.indexOf(filterConfig.hashTags[i]) === -1){
+                            return false;
                         }
                     }
-                    else{
-                        if(photoPosts[i].author === filterConfig.author){
-                            toShow.push(photoPosts[i]);
-                        }
-                    }
-                }
-                else if(filterConfig.createdAt != null){
-                    if(filterConfig.hashTags != null){
-                        if(isCommonHashTags(photoPosts[i], filterConfig.hashTags) && photoPosts[i].createdAt.getDate() === filterConfig.createdAt.getDate() && photoPosts[i].createdAt.getTime() === filterConfig.createdAt.getTime()){
-                            toShow.push(photoPosts[i]);
-                        }
-                    }
-                    else{
-                        if(photoPosts[i].createdAt.getDate() === filterConfig.createdAt.getDate() && photoPosts[i].createdAt.getTime()){
-                            toShow.push(photoPosts[i]);
-                        }
-                    }
-                }
-                else if(filterConfig.hashTags != null){
-                    if(isCommonHashTags(photoPosts[i], filterConfig.hashTags)){
-                        toShow.push(photoPosts[i]);
-                    }
-                }
+                    return true;
+                });
             }
             toShow.sort(compareTo);
             return toShow.slice(skip, skip + top);
         }
     }
 
-    console.log('show first 5 posts with author Alex and date 2019-03-01T17:30:00');
-    console.log(getPhotoPosts(0,5, {author:'Alex', createdAt:new Date('2019-03-01T17:30:00')}));
-
-    console.log('show first 5 posts with author Alex and hashtags love, book');
-    console.log(getPhotoPosts(0,5, {author:'Alex', hashTags: ['love', 'book']}));
-
-    console.log('show first 5 posts with author Alex');
-    console.log(getPhotoPosts(0,5, {author:'Alex'}));
-    
-    console.log('posts 10-18');
-    console.log(getPhotoPosts(10, 8));
-
-    console.log('posts 20-21');
-    console.log(getPhotoPosts(20, 2) + 'nothing to show');
-
-    console.log('get post with id=3');
-    console.log(getPhotoPost('3'));
-
-    console.log('get post with id=23');
-    console.log(getPhotoPost('23'));
-
-    console.log('validation');
-    console.log(validatePhotoPost({id:'1', description:'cool', createdAt:'2018-12-01T17:30:00', photoLink:'image/photo3.jpeg', author:'Me'}));
-    
-    console.log('validation (invalid post)');
-    console.log(validatePhotoPost({id:'1', description:'cool'}));
-
-    console.log('try to add post');
-    console.log(addPhotoPost({id:'21', description:'cool', createdAt:'2018-12-01T17:30:00', photoLink:'image/photo3.jpeg', author:'Me'}));
-
-    console.log('try to add post (invalid post)');
-    console.log(addPhotoPost({id:'22', description:'cool'}));
-
-    console.log(getPhotoPost('21'));
-
-    console.log('try to edit post with id=21');
-    console.log(editPhotoPost('21', {id:'21', description:'good', createdAt:'2018-12-01T17:30:00', photoLink:'image/photo5.jpeg'}));
-
-    console.log(getPhotoPost('21'));
-
-    console.log('try to edit post with id=21');
-    console.log(editPhotoPost('21', {photoLink:'images/photo3.jpeg'}));
-
-    console.log(getPhotoPost('21'));
-
-    console.log('try to remove post with id=21');
-    console.log(removePhotoPost('21'));
-
-    console.log('try to remove post with id=22 (not exists)');
-    console.log(removePhotoPost('22'));
-
+    return {
+        getPhotoPosts: getPhotoPosts,
+        getPhotoPost: getPhotoPost,
+        validatePhotoPost: validatePhotoPost,
+        addPhotoPost: addPhotoPost,
+        editPhotoPost: editPhotoPost,
+        removePhotoPost: removePhotoPost
+    }
 }());
+
+//tests
+
+console.log('show first 5 posts with author Alex and date 2019-03-01T17:30:00');
+console.log(module.getPhotoPosts(0,5, {author:'Alex', createdAt:new Date('2019-03-01T17:30:00')}));
+
+console.log('show first 5 posts with author Alex and hashtags love, book');
+console.log(module.getPhotoPosts(0,5, {author:'Alex', hashTags: ['love', 'book']}));
+
+console.log('show first 5 posts with author Alex');
+console.log(module.getPhotoPosts(0,5, {author:'Alex'}));
+
+console.log('posts 10-18');
+console.log(module.getPhotoPosts(10, 8));
+
+console.log('posts 0-5');
+console.log(module.getPhotoPosts(0, 5));
+
+console.log('posts 20-21');
+console.log(module.getPhotoPosts(20, 2) + 'nothing to show');
+
+console.log('get post with id=3');
+console.log(module.getPhotoPost('3'));
+
+console.log('get post with id=23');
+console.log(module.getPhotoPost('23'));
+
+console.log('validation');
+console.log(module.validatePhotoPost({id:'1', description:'cool', createdAt:new Date ('2018-12-01T17:30:00'), photoLink:'image/photo3.jpeg', author:'Me', state:'active'}));
+
+console.log('validation (invalid post)');
+console.log(module.validatePhotoPost({id:'1', description:'cool'}));
+
+console.log('try to add post');
+console.log(module.addPhotoPost({id:'21', description:'cool', createdAt: new Date ('2018-12-01T17:30:00'), photoLink:'image/photo3.jpeg', author:'Me', state:'active'}));
+
+console.log('try to add post (invalid post)');
+console.log(module.addPhotoPost({id:'22', description:'cool'}));
+
+console.log(module.getPhotoPost('21'));
+
+console.log('try to edit post with id=21');
+console.log(module.editPhotoPost('21', {id:'21', description:'good', createdAt:new Date ('2018-12-01T17:30:00'), photoLink:'image/photo5.jpeg'}));
+
+console.log(module.getPhotoPost('21'));
+
+console.log('try to edit post with id=21');
+console.log(module.editPhotoPost('21', {photoLink:'images/photo3.jpeg'}));
+
+console.log(module.getPhotoPost('21'));
+
+console.log('try to remove post with id=21');
+console.log(module.removePhotoPost('21'));
+
+console.log(module.getPhotoPost('21'));
+
+console.log('try to remove post with id=22 (not exists)');
+console.log(module.removePhotoPost('22'));
